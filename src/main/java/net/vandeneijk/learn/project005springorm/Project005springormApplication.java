1896dd9e6b706e1543c9cc7e33bed9b9396ffd7b
Robert van den Eijk <robert@vandeneijk.net>
2020-04-14 09:31:56 +0200

    Initial commit.

---
+++ src/main/java/net/vandeneijk/learn/project005springorm/Project005springormApplication.java
@@ -0,0 +1,13 @@
+package net.vandeneijk.learn.project005springorm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project005springormApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Project005springormApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/vandeneijk/learn/project005springorm/Project005springormApplicationTests.java
@@ -0,0 +1,13 @@
+package net.vandeneijk.learn.project005springorm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Project005springormApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
